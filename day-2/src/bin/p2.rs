d2c50cc2bc3c8591996834cef9c2e6acd9a642db
arjansunar <arjan.gahatrajsunar@gmail.com>
2024-04-28 20:19:31 +0545

    feat: add setup for day 2

---
+++ day-2/src/bin/p2.rs
@@ -0,0 +1,27 @@
+fn main() {
+    let input = include_str!("./input-p2.txt");
+    dbg!("{}", part2(input));
+}
+
+fn part2(input: &str) -> String {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = part2(
+            "two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen",
+        );
+        assert_eq!(result, "281");
+    }
+}
