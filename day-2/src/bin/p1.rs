6d68008973ebb6bb2dc3559d92a32be4e4b5aa54
arjansunar <arjan.gahatrajsunar@gmail.com>
2024-05-05 07:26:46 +0545

    feat: create parse string implementation for GameSet enum

--- day-2/src/bin/p1.rs
+++ day-2/src/bin/p1.rs
@@ -1,7 +1,39 @@
+use std::str::FromStr;
+
 fn main() {
     let input = include_str!("./input-p1.txt");
 
     dbg!("{}", part1(input));
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum GameSet {
+    Red(u16),
+    Blue(u16),
+    Green(u16),
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct ParseGameSetError;
+
+impl FromStr for GameSet {
+    type Err = ParseGameSetError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts = s.split(' ').collect::<Vec<_>>();
+        let n = parts.first().and_then(|x| x.parse::<u16>().ok());
+        let ball_type = parts.get(1).map(|x| *x);
+
+        if n.is_none() || ball_type.is_none() {
+            return Err(ParseGameSetError);
+        }
+        match ball_type {
+            Some("red") => Ok(GameSet::Red(n.unwrap())),
+            Some("blue") => Ok(GameSet::Blue(n.unwrap())),
+            Some("green") => Ok(GameSet::Green(n.unwrap())),
+            _ => Err(ParseGameSetError),
+        }
+    }
 }
 
 fn part1(input: &str) -> String {
@@ -22,5 +54,12 @@ Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"#,
         );
         assert_eq!(result, "8");
+    }
+
+    #[test]
+    fn parse_game_test() {
+        assert_eq!(GameSet::from_str("3 blue").unwrap(), GameSet::Blue(3));
+        assert_eq!(GameSet::from_str("5 red").unwrap(), GameSet::Red(5));
+        assert_eq!(GameSet::from_str("3 green").unwrap(), GameSet::Green(3));
     }
 }
